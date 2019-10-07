94fef13c670f155dfd42cbe4047196ecce1b9a4e
Eason <tso2381637@gmail.com>
2019-10-07 14:36:52 -0700

    hw2 ed2

--- hw2/longest.py
+++ hw2/longest.py
@@ -1,15 +1,20 @@
 def longest(a):
-	return count_deepth(a[0]) + count_deepth(a[2])
-
+	left = count_deepth(a[0])
+	right = count_deepth(a[2])
+	if max(left[1],right[1]) >= left[0] + right[0]:
+		return max(left[1],right[1])
+	else:
+		return left[0] + right[0]
 def count_deepth(a):
 	if len(a)==1:
-		return 1
+		return [1,0]
 	elif len(a)==0:
-		return 0
+		return [0,0]
 	else:
 		count = 0
 		left = count_deepth(a[0])
 		right = count_deepth(a[2])
-		count += max(left,right) + 1
-		return count
+		longest = max(left[1],right[1])
+		count += max(left[0],right[0]) + 1
+		return [count,longest]
 
