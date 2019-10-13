3bfb9364cbbd19adad6cc465720036a575d09022
Eason <tso2381637@gmail.com>
2019-10-13 15:33:13 -0700

    hw3

--- hw3/closest_sorted.py
+++ hw3/closest_sorted.py
@@ -6,34 +6,31 @@ def find(a,t,k):
 	pivot = bisect.bisect_right(a,t)
 	i = pivot - 1
 	j = pivot
-	result = [None] * len(a)
-	result2 = []
+	result = []
 	while i>=0 and j < len(a) and k>0:
 		if abs(a[i] - t) <= abs(a[j] - t):
-			result[i] = 1
+			result.append(i)
 			i-=1	
 			k-=1		
 		elif abs(a[i] - t) > abs(a[j] - t):
-			result[j] = 1
+			result.append(j)
 			j+=1
 			k-=1
 
 	if k>0:
 		if j==len(a):
 			while(k):
-				result[i]=1
+				result.append(i)
 				i-=1
 				k-=1
 		else:
 			while(k):
-				result[j]=1
+				result.append(j)
 				j+=1
 				k-=1
-	for index,value in enumerate(result):
-		if value == 1:
-			result2.append(a[index])
-
-	return result2
-
+	
+	result.sort()
+	result = [a[i] for i in result]
+	return result
 
 
