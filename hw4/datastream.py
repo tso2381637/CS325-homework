6f586834e6ac686420ec5fed972c5ddfbe60a51f
Eason <tso2381637@gmail.com>
2019-10-21 16:12:31 -0700

    graded question

--- hw4/datastream.py
+++ hw4/datastream.py
@@ -1,12 +1,18 @@
 import heapq
-def ksmallest(k,a):
-	
-	result = a[:k]
-	heapq._heapify_max(result)
+def ksmallest(k, a):
+	a = list(a)
+	result = [-i for i in a[:k]]
+	heapq.heapify(result)
 	for i in a[k:]:
-		print(result)
-		if i<result[0]:
-			heapq._heapreplace_max(result,i)
+		if i<-result[0]:
+			heapq.heapreplace(result,-i)
+
+	result = [-i for i in result]
+	result.reverse()
 	return result
 
 
+
+#print(ksmallest(3, range(1000000, 0, -1)))
+
+#print(ksmallest(4, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
