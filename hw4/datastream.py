bc2b541093c31ea766ebab70dd4313e8e665d1f6
Eason <tso2381637@gmail.com>
2019-10-21 18:51:58 -0700

    graded question

--- hw4/datastream.py
+++ hw4/datastream.py
@@ -3,10 +3,17 @@ def ksmallest(k, a):
 	a = list(a)
 	result = a[:k]
 	heapq._heapify_max(result)
+	result.sort(reverse=True)
 	for i in a[k:]:
-		if i<result[0]:
-			result[0] = i
-			heapq._heapify_max(result)
+		if i<result[0]:			
+			result = result[1:]
+			for j in range(0,len(result)):
+				if j==len(result)-1:
+					result.insert(j+1,i)
+					break
+				elif i<=result[j] and i>=result[j+1]:
+					result.insert(j+1,i)
+
 	result.reverse()
 	return result
 
